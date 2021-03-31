d4b2e7d98dd389dacf41f34e3f614d56891b3bca
JunHyuk <kkjun221@naver.com>
2021-03-31 10:31:35 +0900

    Inner class with this

--- app/src/main/java/com/example/buttonapp325/MyOnClickListener.java
+++ app/src/main/java/com/example/buttonapp325/MyOnClickListener.java
@@ -15,3 +15,14 @@ public class MyOnClickListener implements View.OnClickListener {
        mainActivity.mTextView1.setText("you clicked 김준혁의 button!");
     }
 }
+private class myOnClickListener implements View.OnClickListener {
+    MainActivity mainActivity;
+    public myOnClickListener(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mainActivity.mTextView1.setText("you clicked button 1");
+    }
+}
